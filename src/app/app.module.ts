c7e420012642bfc15ea6258e8117970876c3080c
Yuri Vyatkin <harmoney-yuri@users.noreply.github.com>
2022-03-11 13:28:04 +1300

    [#1] Generate AppRoutingModule ng generate module app-routing --flat --module=app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { MessagesComponent } from './messages/messages.component';
     HeroDetailComponent,
     MessagesComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [
     // no need to place any providers due to the `providedIn` flag...
   ],
