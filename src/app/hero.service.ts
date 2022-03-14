4d7fb4f4a651f86d6da264d7b293487a04d683e2
Yuri Vyatkin <harmoney-yuri@users.noreply.github.com>
2022-03-14 18:12:26 +1300

    [#3] Get hero by id

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -19,7 +19,6 @@ export class HeroService {
   ) {}
 
   /** GET heroes from the server */
-  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl).pipe(
       tap((_) => this.log('fetched heroes')),
@@ -27,12 +26,13 @@ export class HeroService {
     );
   }
 
+  /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
-    // For now, assume that a hero with the specified `id` always exists.
-    // Error handling will be added in the next step of the tutorial.
-    const hero = HEROES.find((h) => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap((_) => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   /**
